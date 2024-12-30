3ffcf49d543bee3ddb939329c43eb8b965d7d903
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-12-30 10:42:19 -0500

    Feat: Sets up i18next for use with en & ja

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { useTranslation } from 'react-i18next'
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  function toggleLanguage() {
+    const currentLanguage = i18n.resolvedLanguage;
+    const newLanguage = currentLanguage === "en" ? "ja" : "en";
+    i18n.changeLanguage(newLanguage);
+  }
 
   return (
     <>
-      <h1>Laurence Agina</h1>
+      <h1>{t('header.myName')}</h1>
+      <button type="button" onClick={toggleLanguage}>Toggle Language</button>
     </>
   )
 }
---
+++ src/i18n.ts
@@ -0,0 +1,21 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import en from "./locales/en.json";
+import ja from "./locales/ja.json";
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    resources: {
+      en: {
+        translation: en,
+      },
+      ja: {
+        translation: ja,
+      },
+    },
+  });
