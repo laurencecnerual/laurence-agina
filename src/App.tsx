9653987d460d93f5826a6f743e7b78c7b9a7cd9e
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-12-30 12:42:36 -0500

    Feat: Adds React Router and sets up basic nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { useTranslation } from 'react-i18next'
+import {Routes, Route, Link} from "react-router-dom";
 
 function App() {
   const { t, i18n } = useTranslation();
@@ -12,8 +13,27 @@ function App() {
 
   return (
     <>
-      <h1>{t('header.myName')}</h1>
-      <button type="button" onClick={toggleLanguage}>Toggle Language</button>
+      <header>
+        <Link to="/">
+          <h1>{t('header.myName')}</h1>
+        </Link>
+        <button type="button" onClick={toggleLanguage}>Toggle Language</button>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/certifications">Certifications</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<h2>Welcome to my portfolio website</h2>} />
+        <Route path="projects" element={<h2>Projects Page</h2>} />
+        <Route path="certifications" element={<h2>Certifications Page</h2>} />
+        <Route path="*" element={<h2>404 Page Not Found</h2>}></Route>
+      </Routes>
+      <footer>
+        <a href="https://www.linkedin.com/in/laurenceagina/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://github.com/laurencecnerual" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </footer>
     </>
   )
 }
