9103c5e2a7a8d1def75dd42e5c3ce15e34344230
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-01-02 16:41:02 -0500

    Feat: Adds translations for footer, styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
+import { useTranslation } from 'react-i18next'
+
 export default function Footer() {
+  const { t } = useTranslation();
+
   return (
     <footer>
-      <a href="https://www.linkedin.com/in/laurenceagina/?locale=en_US" target="_blank" rel="noopener noreferrer">LinkedIn (EN)</a>
-      <a href="https://www.linkedin.com/in/laurenceagina/?locale=ja_JP" target="_blank" rel="noopener noreferrer">LinkedIn (JP)</a>
+      <a href="https://www.linkedin.com/in/laurenceagina/?locale=en_US" target="_blank" rel="noopener noreferrer">LinkedIn ({t('footer.english')})</a>
+      <a href="https://www.linkedin.com/in/laurenceagina/?locale=ja_JP" target="_blank" rel="noopener noreferrer">LinkedIn ({t('footer.japanese')})</a>
       <a href="https://github.com/laurencecnerual" target="_blank" rel="noopener noreferrer">GitHub</a>
     </footer>
   )
