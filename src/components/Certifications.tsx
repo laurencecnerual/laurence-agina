2aedcad8cadff4ea8f1cbce712ada30f27c40991
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-01-16 11:45:29 -0500

    Feat: Enables translations on object key values

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -1,6 +1,36 @@
-import { certifications } from "../data"
+import { Certification } from "../types.ts";
+import { useTranslation } from 'react-i18next'
 
 export default function Certifications() {
+  const { t } = useTranslation();
+  let certifications: Certification[];
+  certifications = [
+    {
+      organization: "Organization 1",
+      title: t('certificationData.certification01.title'),
+      summary: "Summary 1",
+      dateEarned: new Date("2024/08"),
+      dateRenewed: new Date("2024/09"),
+      dateExpiring: new Date("2026/10"),
+      takenInJapanese: false,
+      myScore: 90,
+      maxPossibleScore: 100,
+      certURL: "PDF URL 1"
+    },
+    {
+      organization: "Organization 2",
+      title: t('certificationData.certification02.title'),
+      summary: "Summary 2",
+      dateEarned: new Date("2024/09"),
+      dateRenewed: new Date("2024/10"),
+      dateExpiring: new Date("2026/11"),
+      takenInJapanese: false,
+      myScore: 90,
+      maxPossibleScore: 100,
+      certURL: "PDF URL 2"
+    },
+  ];
+
   return (
     <>
       <h2>Certifications Page</h2>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,35 @@
-import { projects } from "../data"
+import { Project } from "../types.ts";
+import { useTranslation } from 'react-i18next'
 
 export default function Projects() {
+  const { t } = useTranslation();
+  let projects: Project[];
+
+  projects = [
+    {
+      title: t('projectData.project01.title'),
+      summary: "Summary 1",
+      contributions: ["Contribution 1.1", "Contribution 1.2", "Contribution 1.3"],
+      role: "Role 1",
+      teamSize: 4,
+      startDate: new Date("2024/11"),
+      endDate: new Date("2024/11"),
+      gitHubURL: "GitHub URL 1",
+      deploymentURL: "Render URL 1"
+    },
+    {
+      title: t('projectData.project02.title'),
+      summary: "Summary 2",
+      contributions: ["Contribution 2.1", "Contribution 2.2", "Contribution 2.3"],
+      role: "Role 2",
+      teamSize: 1,
+      startDate: new Date("2024/12"),
+      endDate: new Date("2024/12"),
+      gitHubURL: "GitHub URL 2",
+      deploymentURL: "Render URL 2"
+    },
+  ];
+
   return (
     <>
       <h2>Projects Page</h2>
