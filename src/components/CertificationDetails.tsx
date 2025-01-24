27404d640e04fb1cf6d7da7698814011bf091873
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-01-25 08:12:02 +0900

    Fix: Moves back button outside of content

--- src/components/CertificationDetails.tsx
+++ src/components/CertificationDetails.tsx
@@ -16,17 +16,19 @@ export default function CertificationDetails() {
   }, [i18n.resolvedLanguage]);
 
   return (
-    <div className="certification-details">
+    <>
       <Link to="/certifications"><p>{t('navigation.back')}</p></Link>
-      <h2 className="title">{certification?.title}{certification?.abbreviation && ` (${certification?.abbreviation})`}</h2>
-      <p className="summary">{certification?.summary}</p>
-      { certification?.takenInJapanese && <p className="takenInJapanese">{t('certificationsPage.takenInJapaneseLabel')}</p> }
-      <p className="organization">{t('certificationsPage.organizationLabel')}: {certification?.organization}</p>
-      <p className="dateEarned">{t('certificationsPage.dateEarnedLabel')}: {certification?.dateEarned}</p>
-      { certification?.dateRenewed && <p className="dateRenewed">{t('certificationsPage.dateRenewedLabel')}: {certification?.dateRenewed}</p> }
-      { certification?.dateExpiring && <p className="dateExpiring">{t('certificationsPage.dateExpiringLabel')}: {certification?.dateExpiring}</p> }
-      { certification?.myScore && <p className="score">{"" + certification?.myScore}{certification?.maxPossibleScore && " / " + certification?.maxPossibleScore} {t('certificationsPage.points')}</p> }
-      <p className="certURL">{t('certificationsPage.certURLLabel')}: <a href={certification?.certURL} target="_blank" rel="noopener noreferrer">{certification?.certURL}</a></p>
-    </div>
+      <div className="certification-details">
+        <h2 className="title">{certification?.title}{certification?.abbreviation && ` (${certification?.abbreviation})`}</h2>
+        <p className="summary">{certification?.summary}</p>
+        { certification?.takenInJapanese && <p className="takenInJapanese">{t('certificationsPage.takenInJapaneseLabel')}</p> }
+        <p className="organization">{t('certificationsPage.organizationLabel')}: {certification?.organization}</p>
+        <p className="dateEarned">{t('certificationsPage.dateEarnedLabel')}: {certification?.dateEarned}</p>
+        { certification?.dateRenewed && <p className="dateRenewed">{t('certificationsPage.dateRenewedLabel')}: {certification?.dateRenewed}</p> }
+        { certification?.dateExpiring && <p className="dateExpiring">{t('certificationsPage.dateExpiringLabel')}: {certification?.dateExpiring}</p> }
+        { certification?.myScore && <p className="score">{"" + certification?.myScore}{certification?.maxPossibleScore && " / " + certification?.maxPossibleScore} {t('certificationsPage.points')}</p> }
+        <p className="certURL">{t('certificationsPage.certURLLabel')}: <a href={certification?.certURL} target="_blank" rel="noopener noreferrer">{certification?.certURL}</a></p>
+      </div>
+    </>
   )
 }
--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -14,20 +14,22 @@ export default function ProjectDetails() {
   }, [i18n.resolvedLanguage]);
 
   return (
-    <div className="project-details">
+    <>
       <Link to="/projects"><p>{t('navigation.back')}</p></Link>
-      <h2 className="title">{project?.title}</h2>
-      <p className="summary">{project?.summary}</p>
-      { project?.role && <p className="role">{t('projectsPage.roleLabel')}: {project?.role}</p> }
-      <p className="teamSize">{t('projectsPage.teamSizeLabel')}: {"" + project?.teamSize}</p>
-      <p className="stack">{t('projectsPage.stackLabel')}: {project?.stack}</p>
-      <p className="dates">{project?.startDate} ~{project?.endDate && ` ${project.endDate}`}</p>
-      <p>{t('projectsPage.contributionsLabel')}:</p>
-      <ul className="contributions">
-        {project?.contributions.map((contribution, index) => <li key={index}>{contribution}</li>)}
-      </ul>
-      <p className="gitHubURL">{t('projectsPage.gitHubURLLabel')}: <a href={project?.gitHubURL} target="_blank" rel="noopener noreferrer">{project?.gitHubURL}</a></p>
-      { project?. deploymentURL && <p className="deploymentURL">{t('projectsPage.deploymentURLLabel')}: <a href={project?.deploymentURL} target="_blank" rel="noopener noreferrer">{project?.deploymentURL}</a></p> }
-    </div>
+      <div className="project-details">
+        <h2 className="title">{project?.title}</h2>
+        <p className="summary">{project?.summary}</p>
+        { project?.role && <p className="role">{t('projectsPage.roleLabel')}: {project?.role}</p> }
+        <p className="teamSize">{t('projectsPage.teamSizeLabel')}: {"" + project?.teamSize}</p>
+        <p className="stack">{t('projectsPage.stackLabel')}: {project?.stack}</p>
+        <p className="dates">{project?.startDate} ~{project?.endDate && ` ${project.endDate}`}</p>
+        <p>{t('projectsPage.contributionsLabel')}:</p>
+        <ul className="contributions">
+          {project?.contributions.map((contribution, index) => <li key={index}>{contribution}</li>)}
+        </ul>
+        <p className="gitHubURL">{t('projectsPage.gitHubURLLabel')}: <a href={project?.gitHubURL} target="_blank" rel="noopener noreferrer">{project?.gitHubURL}</a></p>
+        { project?. deploymentURL && <p className="deploymentURL">{t('projectsPage.deploymentURLLabel')}: <a href={project?.deploymentURL} target="_blank" rel="noopener noreferrer">{project?.deploymentURL}</a></p> }
+      </div>
+    </>
   )
 }
