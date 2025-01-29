2a9489d14b786a81ab5f5c0a2f91cca2fea3e3c6
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-01-29 14:43:54 +0900

    Feat: Adds contents to 404 page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,3 +1,18 @@
+import { useTranslation } from 'react-i18next';
+
 export default function NotFound() {
-  return <h2>404 Page Not Found</h2>
+  const { t, i18n } = useTranslation();
+
+  return (
+    <div className="not-found">
+      <h2 className={i18n.resolvedLanguage === "ja" ? "japanese" : ""}>{t('notFoundPage.404')}</h2>
+      <iframe 
+        style={{aspectRatio: '16/9'}}
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1" 
+        title="YouTube video player" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" 
+        allowFullScreen>
+      </iframe>
+    </div>
+  )
 }
