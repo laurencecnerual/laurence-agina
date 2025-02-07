e5680b84ca323136a9f7fd5fa243e80f2b027c26
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-02-07 15:36:11 +0900

    Feat: Adds icons to certification links

--- src/components/CertificationDetails.tsx
+++ src/components/CertificationDetails.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { Certification } from "../types";
 import { useTranslation } from 'react-i18next'
 import { Link, useParams } from "react-router-dom";
+import Icon from '@mdi/react';
+import { mdiCertificate, mdiInformation } from '@mdi/js';
 
 export default function CertificationDetails() {
   const { t, i18n } = useTranslation();
@@ -27,8 +29,8 @@ export default function CertificationDetails() {
         { certification?.dateExpiring && <p className="dateExpiring">{t('certificationsPage.dateExpiringLabel')}: {certification?.dateExpiring}</p> }
         { Number(certification?.myScore) > 0 && <p className="score">{"" + certification?.myScore}{certification?.maxPossibleScore && " / " + certification?.maxPossibleScore} {t('certificationsPage.points')}</p> }
         { certification?.takenInJapanese && <p className="takenInJapanese">{t('certificationsPage.takenInJapaneseLabel')}</p> }
-        <p className="moreInfoURL"><a href={certification?.moreInfoURL} target="_blank" rel="noopener noreferrer">{t('certificationsPage.moreInfoURLLabel')}</a></p>
-        <p className="certURL"><a href={certification?.certURL} target="_blank" rel="noopener noreferrer">{t('certificationsPage.certURLLabel')}</a></p>
+        <p className="moreInfoURL"><a href={certification?.moreInfoURL} target="_blank" rel="noopener noreferrer">{t('certificationsPage.moreInfoURLLabel')} <Icon path={mdiInformation} size={1} /></a></p>
+        <p className="certURL"><a href={certification?.certURL} target="_blank" rel="noopener noreferrer">{t('certificationsPage.certURLLabel')} <Icon path={mdiCertificate} size={1} /></a></p>
       </div>
     </>
   )
