14d73e711bd54ed2aa55aa59bb11ae673a0f46ea
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-05-26 18:26:44 +0900

    fix: Adds transition to home page picture

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next'
 
 export default function Home() {
   const { t, i18n } = useTranslation();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  };
+  
 
   return (
     <>
       <h2 className={i18n.resolvedLanguage === "ja" ? " title home-text japanese" : "title home-text"}>{t('homePage.title')}</h2>
-      <img className="my-photo" src="/me.jpg" alt="A photo of me"/>
+      <img className="my-photo" src="/me.jpg" alt="A photo of me" onLoad={handleImageLoad}
+        style={{
+          opacity: isLoaded ? 1 : 0,
+          transition: 'opacity 1s ease-in-out',
+        }}/>
       <p className="summary home-text">{t('homePage.summary')}</p>
       <p className="work home-text">{t('homePage.work')}</p>
       <p className="skills home-text">{t('homePage.skills')}</p>
